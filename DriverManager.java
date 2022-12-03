bd4a64d0990f2a18427c888eb01d732f96c8db52
saimanasaB <91835638+saimanasaB@users.noreply.github.com>
2022-12-03 16:11:34 +0530

    Add files via upload

---
+++ DriverManager.java
@@ -0,0 +1,19 @@
+
+import com.sun.jdi.connect.spi.Connection;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lenovo
+ */
+class DriverManager {
+
+    static Connection getConnection(String jdbcmysqllocalhost3306mydbuseSSLfalse, String root, String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ ResultSet.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lenovo
+ */
+class ResultSet {
+
+    boolean next() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Object getInt(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String getString(String username) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
